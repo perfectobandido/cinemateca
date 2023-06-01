a6ea7b0498cabbdfbf810a54bd847de3d12e6483
Andres Gonzalez <zorrochileno@gmail.com>
2023-06-01 00:39:39 -0400

    correccion final

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Movie.create(name: 'Movie 1', synopsis: 'Synopsis 1', director: 'Director 1')
-Movie.create(name: 'Movie 2', synopsis: 'Synopsis 2', director: 'Director 2')
+#   Movie.create(name: 'Movie 1', synopsis: 'Synopsis 1', director: 'Director 1')
+#   Movie.create(name: 'Movie 2', synopsis: 'Synopsis 2', director: 'Director 2')
 
-Serie.create(name: 'Serie 1', synopsis: 'Synopsis 1', director: 'Director 1')
-Serie.create(name: 'Serie 2', synopsis: 'Synopsis 2', director: 'Director 2')
+#   Serie.create(name: 'Serie 1', synopsis: 'Synopsis 1', director: 'Director 1')
+#   Serie.create(name: 'Serie 2', synopsis: 'Synopsis 2', director: 'Director 2')
 
-DocumentaryFilm.create(name: 'Documentary Film 1', synopsis: 'Synopsis 1', director: 'Director 1')
-DocumentaryFilm.create(name: 'Documentary Film 2', synopsis: 'Synopsis 2', director: 'Director 2')
+#   DocumentaryFilm.create(name: 'Documentary Film 1', synopsis: 'Synopsis 1', director: 'Director 1')
+#   DocumentaryFilm.create(name: 'Documentary Film 2', synopsis: 'Synopsis 2', director: 'Director 2')
+10.times do |i|
+    Movie.create(name: Faker::Movie.title, synopsis: Faker::Movie.quote, director: Faker::Name.name)
+    Serie.create(name: Faker::Movie.title, synopsis: Faker::Movie.quote, director: Faker::Name.name)
+    DocumentaryFilm.create(name: Faker::Movie.title, synopsis: Faker::Movie.quote, director: Faker::Name.name)
+end
